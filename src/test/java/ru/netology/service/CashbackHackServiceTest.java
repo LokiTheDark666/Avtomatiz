07eb733cf1a5fce15dc63683167245c381eb4b97
LokiTheDark666 <khcr.loki@mail.ru>
2022-07-22 15:57:47 +0300

    add testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,25 @@
+package ru.netology.service;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void testRemain() {
+
+        CashbackHackService service = new CashbackHackService();
+        int expected = 100;
+        int results = service.remain(900);
+        assertEquals(expected,results);
+    }
+
+    @Test
+    public void testRemain2к(){
+        CashbackHackService service = new CashbackHackService();
+        int expected = 100;
+        int results = service.remain(1900);
+        assertEquals(expected,results);
+    }
+}
